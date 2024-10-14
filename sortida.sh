7f8ffe98e7ba48879543f5fdd9af8a8649cc7a58
elsa <elsa@uab.com>
2024-10-14 08:23:59 +0200

    act 4 i 5

--- sortida.sh
+++ sortida.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+#Activitat 1
+
 cut -d ',' -f 1-11,13-15 supervivents.csv > temps.csv
+
+#Activitat 2
 
 awk -F ',' '{if ($14 == "False") print $0}' temps.csv > error.csv
 
@@ -9,18 +13,30 @@ y=$(wc -l < temps.csv)
 z=$(($y - $x)) 
 echo $z
 
+#Activitat 3
 
 awk -F ',' '{if ($8 < 1000000) print $0 ",Bo";
              else
                 if ($8 <= 10000000) print $0 ",Excel·lent";
                 else print $0 ",Estrella";}' error.csv > views.csv
 
+#Activitat 4
 
-while [[]]
-   cut -d ',' '($9*100)/$8 echo $16'
-done
+{
+while [[cut -d ',' read -r '$8 $9 $10' ]]
+    if [ '$8' != 0 ]; then
+        Rlikes='($9*100)/$8 echo $16'
+        Rdislikes='($10*100)/$8 echo $17'
+    else
+        Rlikes=0
+        Rdislikes=0
+    fi
+}views.csv > likes.csv
 
-while[[]]
-   cut -d ',' '($10*100)/$8 echo $17'
-done 
-   
+#Activitat 5
+read -p "Introdueix el titol o l'identificador del video: " video
+
+if (grep -w video
+      echo '$3 $6 $8 $9 $10 $15 $16 $17'
+else
+      echo "No s'han trobat coincidencies"
