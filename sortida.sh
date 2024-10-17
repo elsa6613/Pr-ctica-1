8fbd4a3b4df9a253ec7fe73d2595e877ac84fba6
elsa <elsa@uab.com>
2024-10-17 13:41:37 +0200

    correccions

--- sortida.sh
+++ sortida.sh
@@ -8,10 +8,10 @@ cut -d ',' -f 1-11,13-15 supervivents.csv > temps.csv
 
 awk -F ',' '{if ($14 == "False") print $0}' temps.csv > error.csv
 
-x=$(wc -l < error.csv) 
-y=$(wc -l < temps.csv) 
-z=$(($y - $x)) 
-echo $z
+x=$(wc -l < error.csv)
+y=$(wc -l < temps.csv)
+z=$(($y - $x -1))
+echo "$z"
 
 #Activitat 3
 
@@ -23,11 +23,11 @@ awk -F ',' '{if ($8 < 1000000) print $0 ",Bo";
 #Activitat 4
 
 while read -r linia; do
-    views=$(echo $linia | cut -d ',' -f 8)
-    likes=$(echo $linia | cut -d ',' -f 9)
-    dislikes=$(echo $linia | cut -d ',' -f 10)
+    views=$(echo "$linia" | cut -d ',' -f 8)
+    likes=$(echo "$linia" | cut -d ',' -f 9)
+    dislikes=$(echo "$linia" | cut -d ',' -f 10)
 
-    if [ views != 0 ]; then
+    if [ "$views" -ne 0 ]; then
         Rlikes=$(((likes*100)/views))
         Rdislikes=$(((dislikes*100)/views))
     else
@@ -39,16 +39,27 @@ done < views.csv
 
 #Activitat 5
 
-if [[ ! -f sortida.csv]]; then
+read -p "Introdueix el titol o l'identificador del video: " video
+
+if [[ ! -f likes.csv ]]; then
     echo "L'arxiu sortida.csv no existeix"
     exit 1
 fi
 
-read -p "Introdueix el titol o l'identificador del video: " video
+resultat=$(grep -i "$video" likes.csv)
 
-resultat=$(grep -i video sortida.csv)
-
-if [ -n "$resultat" ];then
-      echo '$3,$6,$8,$9,$10,$15,$16,$17' > final.csv
+if [[ -n "$resultat" ]];then
+	while read -r linia; do	
+      		Title=$(echo $linia | cut -d ',' -f 3) 
+      		Publish_Time=$(echo $linia | cut -d ',' -f 6)
+      		views=$(echo $linia | cut -d ',' -f 8)
+      		likes=$(echo $linia | cut -d ',' -f 9)
+      		dislikes=$(echo $linia | cut -d ',' -f 10)
+      		Ranking_views=$(echo $linia | cut -d ',' -f 15)
+      		Rlikes=$(echo $linia | cut -d ',' -f 16)
+      		Rdislikes=$(echo $linia | cut -d ',' -f 17)
+      		echo "$Title,$Publish_Time,$views,$likes,$dislikes,$Ranking_views,$Rlikes,$Rdislikes" >> final.csv
+	done < likes.csv
 else
       echo "No s'han trobat coincidències." > final.csv
+fi
